6a9bef37b11b94e7adb6bb4aa22ed69c888c9070
jpansare <jaypansare96@gmail.com>
2021-03-18 18:12:17 +0530

    build using parcel

---
+++ dist/index.b7fe6133.js
@@ -0,0 +1,2 @@
+!function(){const e=document.getElementsByClassName("toggle-button")[0],t=document.getElementsByClassName("navbar-links")[0];e.addEventListener("click",(()=>{t.classList.toggle("active")}))}();
+//# sourceMappingURL=index.b7fe6133.js.map
