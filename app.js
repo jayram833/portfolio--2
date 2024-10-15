f7ac8f0bf60014aaeaddad85e35592debc044acf
jayram833 <jaypansare96@gmail.com>
2024-10-15 11:24:32 +0530

    added experience section and added a theme colors

--- app.js
+++ app.js
@@ -7,15 +7,9 @@ let isDarkMode = false;
 
 checkbox.addEventListener("change", () => {
   if (isDarkMode) {
-    // Switching to light mode
     document.body.classList.remove("dark-mode");
-    sun.classList.remove("sunset");
-    sun.classList.add("sunrise");
   } else {
-    // Switching to dark mode
     document.body.classList.add("dark-mode");
-    sun.classList.remove("sunrise");
-    sun.classList.add("sunset");
   }
 
   isDarkMode = !isDarkMode;
